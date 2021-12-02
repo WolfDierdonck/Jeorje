f4c2c0d0ee1546a9f345885368d4758a80c26477
Yashvardhan Mulki <yash.mulki@hotmail.com>
2021-12-02 03:37:55 -0500

    "If a tree ~falls~ collapses in a forest and no one is around to hear it, does it make a sound?"

--- Jeorje/Parser.cs
+++ Jeorje/Parser.cs
@@ -1,7 +1,63 @@
+using System.Collections.Generic;
+
 namespace Jeorje
 {
     public class Parser
     {
-        
+        private static List<TokenType> collapsibleOperators = new List<TokenType>
+        {
+            TokenType.And,
+            TokenType.Or
+        };
+
+        public static AST collapseTree(BinaryAST input)
+        {
+            if (null == input)
+            {
+                return null;
+            }
+            
+            if (input.Token.IsOperator)
+            {
+                // Operator, interesting stuff here
+                if (collapsibleOperators.Contains(input.Token.TokenType))
+                {
+                    // Only consider the right node because the left node cannot be pulled in
+                    return CollapseOperator(input);
+                }
+                else
+                {
+                    AST newNode = new AST(input.Token);
+                    var left = collapseTree(input.left);
+                    var right = collapseTree(input.right);
+                    newNode.Children.Add(left);
+                    newNode.Children.Add(right);
+                    return newNode;
+                }
+            }
+            else
+            {
+                return new AST(input.Token); 
+            }
+        }
+
+        private static AST CollapseOperator(BinaryAST input)
+        {   
+            var left = collapseTree(input.left);
+            var right = input.right;
+            AST newNode = new AST(input.Token);
+            newNode.Children.Add(left);
+
+            var rightTmp = right;
+            while (rightTmp.Token.TokenType == input.Token.TokenType)
+            {
+                newNode.Children.Add(collapseTree(rightTmp.left));
+                rightTmp = rightTmp.right;
+            }
+
+            newNode.Children.Add(collapseTree(rightTmp)); 
+            return newNode;
+        }
+
     }
 }
