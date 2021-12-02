1d66ab1e6fcefe9637d2656760af8bbb954567e9
Yashvardhan Mulki <yash.mulki@hotmail.com>
2021-12-01 21:02:41 -0500

    Initial Project Setup

---
+++ Jeorje/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jeorje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
